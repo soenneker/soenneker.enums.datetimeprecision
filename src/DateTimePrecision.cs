183d0969cad5b705af4a1af1d3cdb4463afd702f
Jake Soenneker <jake@soenneker.com>
2024-03-23 10:57:05 -0500

    Expands enum

--- src/DateTimePrecision.cs
+++ src/DateTimePrecision.cs
@@ -7,12 +7,18 @@ namespace Soenneker.Enums.DateTimePrecision;
 /// </summary>
 public sealed class DateTimePrecision : SmartEnum<DateTimePrecision>
 {
-    public static readonly DateTimePrecision Second = new(nameof(Second), 0);
-    public static readonly DateTimePrecision Minute = new(nameof(Minute), 1);
-    public static readonly DateTimePrecision Hour = new(nameof(Hour), 2);
-    public static readonly DateTimePrecision Day = new(nameof(Day), 3);
-    public static readonly DateTimePrecision Month = new(nameof(Month), 4);
-    public static readonly DateTimePrecision Year = new(nameof(Year), 5);
+    public static readonly DateTimePrecision Nanosecond = new(nameof(Nanosecond), 0);
+    public static readonly DateTimePrecision Microsecond = new(nameof(Microsecond), 1);
+    public static readonly DateTimePrecision Millisecond = new(nameof(Millisecond), 2);
+    public static readonly DateTimePrecision Second = new(nameof(Second), 3);
+    public static readonly DateTimePrecision Minute = new(nameof(Minute), 4);
+    public static readonly DateTimePrecision Hour = new(nameof(Hour), 5);
+    public static readonly DateTimePrecision Day = new(nameof(Day), 6);
+    public static readonly DateTimePrecision Week = new(nameof(Week), 7);
+    public static readonly DateTimePrecision Month = new(nameof(Month), 8);
+    public static readonly DateTimePrecision Quarter = new(nameof(Quarter), 9);
+    public static readonly DateTimePrecision Year = new(nameof(Year), 10);
+    public static readonly DateTimePrecision Decade = new(nameof(Decade), 11);
 
     private DateTimePrecision(string name, int value) : base(name, value)
     {
